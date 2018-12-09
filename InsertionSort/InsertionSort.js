02b2213a7e0bf1baa1fdb980cb59cbdf6ca0aac3
yurickyh <yurickyussuke@hotmail.com>
2018-12-09 16:11:18 -0200

    Creating Insertion Sort algorithm in javascript.
    Signed-off-by: yurickyh <yurickyussuke@hotmail.com>

---
+++ InsertionSort/InsertionSort.js
@@ -0,0 +1,18 @@
+function main(){
+    var v = [3,4,2,1,6,5]; //array of values to be sorted
+    insertionSort(v); //exec insertion sort
+    console.log(v); //show output
+}
+
+function insertionSort(v){
+    for(var i = 1; i < v.length; i++){
+        const pivot = v[i]; //pivot stores the current value
+        var j = i-1;
+        for(j = i-1; pivot < v[j] && j >= 0; j--){
+            v[j+1] = v[j];
+        }
+        v[j+1] = pivot; //insert value in the correct position
+    }
+}
+
+main();
